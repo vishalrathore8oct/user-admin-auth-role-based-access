fd8c2302feb5741fb46672238addaec8e0944af3
vishalrathore8oct <vishal.rathore8oct@gmail.com>
2024-12-29 13:17:40 +0530

    write the user register controller body logic for successfull registration

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,17 +1,47 @@
 const User = require('../models/user.model.js');
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
 
 const registerUser = async (req, res) => {
+    try {
+        const { name, email, password, role } = req.body;
 
-    res.send('Register Page Success');
+        // console.log(name, email, password);
+
+        if (!name || !email || !password) {
+            return res.status(400).send('Please fill all the fields');
+        }
+
+        const userExist = await User.findOne({ email });
+
+        // console.log(userExist);
+
+        if (userExist) {
+            return res.status(400).send('User already exist');
+        }
+
+        const user = await User.create({
+            name,
+            email,
+            password,
+            role
+        });
+
+        res.status(201).json({ message: 'User registered successfully', id: user._id, name: user.name, email: user.email, role: user.role });
+
+    } catch (error) {
+
+        res.status(500).json({ message: 'Error registering user', error });
+
+    }
+
+
 
 }
 
 const loginUser = async (req, res) => {
 
     res.send('Login Page Success');
-    
+
 }
 
 module.exports = {
